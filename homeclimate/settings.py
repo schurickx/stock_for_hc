90b099cb661b5cf5f4060b271ec5b9d64b5fe4d2
aleksandr <schurickx@bk.ru>
2021-11-29 15:45:06 +0300

    work create

--- homeclimate/settings.py
+++ homeclimate/settings.py
@@ -112,6 +112,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+
     # 'DEFAULT_PARSER_CLASSES': [
     #     'rest_framework.parsers.JSONParser',
     # ],
--- inventory/views.py
+++ inventory/views.py
@@ -1,15 +1,14 @@
-from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Provider, Position, Category, Entity, Invoice, Operation, OperationDetail, Stock
 from .serializers import PositionSerializer, ProviderSerializer, CategorySerializer, EntitySerializer, \
     InvoiceSerializer, OperationSerializer, OperationDetailSerializer, StockSerializer
-from rest_framework.authentication import BasicAuthentication
-
 from .utils import CsrfExemptSessionAuthentication
 
 
@@ -76,6 +75,15 @@ class OperationViewSet(ModelViewSet):
         serializer.save(user=self.request.user)
 
 
+# class OperationDetailViewSet(ModelViewSet):
+#     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+#     queryset = OperationDetail.objects.all()
+#     serializer_class = OperationDetailSerializer
+#     permission_classes = [IsAuthenticated]
+#     filter_backends = [DjangoFilterBackend, OrderingFilter]
+#     filterset_fields = ('operation', 'position',)
+#     ordering_fields = ('operation',)
+
 class OperationDetailViewSet(ModelViewSet):
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     queryset = OperationDetail.objects.all()
@@ -85,7 +93,43 @@ class OperationDetailViewSet(ModelViewSet):
     filterset_fields = ('operation', 'position',)
     ordering_fields = ('operation',)
 
+    def perform_create(self, serializer):
+        data = serializer.validated_data
+        position = data.get('position')
+        entity = data.get('entity')
+        invoice = data.get('invoice')
+        price = data.get('price')
+        quantity = data.get('quantity')
+        type_operation = data.get('operation').kind
+        instance = Stock.objects.filter(position=position, entity=entity,
+                                        invoice=invoice, price=price)
 
+        def is_exists(obj):
+            if not obj.exists():
+                raise ValidationError('Данной позиции нет в наличии на складе')
+            return True
+
+        def quantity_update(q_set, total):
+            q_set.update(quantity=total)
+            for q in q_set:
+                q.save()
+
+        if type_operation == 'IN':
+            if instance.exists():
+                quantity = instance[0].quantity + quantity
+                quantity_update(instance, quantity)
+            else:
+                stock_serializer = StockSerializer(data=self.request.data)
+                stock_serializer.is_valid(raise_exception=True)
+                stock_serializer.save()
+        if type_operation == 'OUT':
+            if is_exists(instance) and instance[0].quantity > 0 and instance[0].quantity >= quantity:
+                quantity = instance[0].quantity - quantity
+                quantity_update(instance, quantity)
+            else:
+                raise ValidationError('Количество позиций для списания превышает наличие на складе')
+
+        super().perform_create(serializer)
 
 
 # class StockViewSet(ReadOnlyModelViewSet):
